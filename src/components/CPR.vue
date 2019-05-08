e8dd8069af71af5587984ca26def7b51046f58ad
zyhxym <zyhxym@gmail.com>
2019-05-08 11:26:45 +0800

    照片实验

--- src/components/CPR.vue
+++ src/components/CPR.vue
@@ -4,28 +4,41 @@
         <mt-button size="small" type="danger" slot="left" icon="back"
             @click="returnA()"><small>返回</small></mt-button>
       <mt-button size="small" slot="right"
-            @click="takephoto()"><small>拍照</small></mt-button>
+            @click="choosephoto()"><small>拍照</small></mt-button>
       </mt-header>
       <br><br>
-      <div><img id="image" height="300px"></img></div>
+      <div><img id="image" style="max-height: 200px; max-width: 90%;" ></img></div>
+      <button @click="uploadPicture()"><small>上传</small></button>
+      <div><img id="image1" style="max-height: 200px; max-width: 90%;" :src="photosrc" ></img></div>
     <router-view></router-view>
     </div>
 </template>
 
 <script>
+import global from './global.vue'
 export default {
+  data(){
+    return {
+      photosrc: global.photoUrl+"zyh_1557216080825test.jpg"
+    }
+  },
+  mounted(){
+    console.log(document.getElementById('image1').src)
+
+
+  },
   methods: {
     returnA() {
       this.$router.push({name: '病人列表',params:{SELECTED:"病人"}});
     },
     choosephoto() {
-      navigator.camera.getPicture(onSuccess, onFail, { 
-        quality: 100,
-        destinationType: 1,
-        sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
-      // targetWidth: 300,
-      // targetHeight: 300
-    });
+    //   navigator.camera.getPicture(onSuccess, onFail, { 
+    //     quality: 100,
+    //     destinationType: 1,
+    //     sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+    //   // targetWidth: 300,
+    //   // targetHeight: 300
+    // });
 
       function onSuccess(imageData) {
         document.getElementById('image').src =imageData;
@@ -37,12 +50,12 @@ export default {
 
     },
     takephoto() {
-      navigator.camera.getPicture(onSuccess, onFail, { 
-        quality: 100,
-        destinationType: 1,
-      // targetWidth: 300,
-      // targetHeight: 300
-    });
+    //   navigator.camera.getPicture(onSuccess, onFail, { 
+    //     quality: 100,
+    //     destinationType: 1,
+    //   // targetWidth: 300,
+    //   // targetHeight: 300
+    // });
 
       function onSuccess(imageData) {
         document.getElementById('image').src =imageData;
@@ -54,22 +67,25 @@ export default {
 
     },
     uploadPicture() {
-      var temp_photoaddress = Storage.get('USERID') + '_' + new Date().getTime() + 'test.jpg'
-      var uri = encodeURI("101.132.140.25:3000")
+      var imgURI = document.getElementById('image').src
+      var photoname = window.localStorage.getItem('USERID') + '_' + new Date().getTime() + 'test.jpg'
+      var uri = encodeURI(global.uploadUrl)
       var options = {
         fileKey: 'file',
-        fileName: temp_photoaddress,
+        fileName: photoname,
         chunkedMode: true,
         mimeType: 'image/jpeg'
       }
-      var ft = new FileTransfer();
-      ft.upload(imgURI, uri, success, fail, options)
-      var success = function (r) {
+      function success(r) {
+        console.log(r)
         alert("上传成功! Code = " + r.responseCode);
       }
-      var fail = function (error) {
+      function fail(error) {
         alert("上传失败! Code = " + error.code);
       }
+      var ft = new FileTransfer();
+      ft.upload(imgURI, uri, success, fail, options)
+      
     },
 
   },
---
+++ src/components/global.vue
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+    // 定义一些公共的属性和方法
+    const photoUrl = 'http://101.132.140.25:8001/photos/'
+    const uploadUrl = "http://101.132.140.25:3000/upload"
+    function commonFun() {
+        console.log("公共方法")
+    }
+    // 暴露出这些属性和方法
+    export default {
+        photoUrl,
+        commonFun
+    }
+</script>
