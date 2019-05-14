ee57a7c03a2f2813f985f094611721eeb5c80419
zyhxym <zyhxym@gmail.com>
2019-05-14 17:28:01 +0800

    没用的东西，注掉了

--- src/components/CPR.vue
+++ src/components/CPR.vue
@@ -10,6 +10,12 @@
       <div><img v-gallery id="image" style="max-height: 200px; max-width: 90%;" ></img></div>
       <mt-button type="primary" @click="uploadPicture()"><small>上传</small></mt-button>
       <div><img id="image1" v-gallery style="max-height: 200px; max-width: 90%;" :src="photosrc" ></img></div>
+      <!-- <mt-button type="primary" @click="startRecord()"><small>开始</small></mt-button>
+      <mt-button type="primary" @click="stopRecord()"><small>结束</small></mt-button>
+      <mt-button type="primary" @click="start()"><small>播放</small></mt-button>
+      <mt-button type="primary" @click="stop()"><small>停止</small></mt-button>
+      <mt-button type="primary" @click="uploadRecord()"><small>上传</small></mt-button>
+      <mt-button type="primary" @click="convertRecord()"><small>识别</small></mt-button> -->
     <router-view></router-view>
     </div>
 </template>
@@ -19,12 +25,97 @@ import global from './global.vue'
 export default {
   data(){
     return {
-      photosrc: global.photoUrl+"zyh_1557216080825test.jpg"
+      photosrc: global.photoUrl+"zyh_1557216080825test.jpg",
+      // mediaRec:{},
+      // mediaRec1:{}
     }
   },
   mounted(){
+    //this.initaudio()
   },
   methods: {
+    // initaudio(){
+    //   var src = "myrecording.mp3";
+    // this.mediaRec = new Media(src,
+    //     // success callback
+    //     function() {
+    //         console.log("recordAudio():Audio Success");
+    //     },
+ 
+    //     // error callback
+    //     function(err) {
+    //         console.log("recordAudio():Audio Error: "+ err.code);
+    //     });
+    // resolveLocalFileSystemURL(cordova.file.externalRootDirectory+"myrecording.mp3",function(entry) {
+    //     console.log('cdvfile URI: ' + entry.toInternalURL());
+    //     console.log('URI: ' + entry.toURL());
+    //     var audioURI = entry.toURL();
+    //     this.mediaRec1 = new Media(audioURI,
+    //     // success callback
+    //     function() {
+    //         console.log("Audio Success");
+    //     },
+ 
+    //     // error callback
+    //     function(err) {
+    //         console.log("Audio Error: "+ err.code);
+    //     });
+
+    //   }, function (err) {
+    //     console.log(err)
+    //   });
+
+    
+
+    
+    // },
+
+    // uploadRecord(){
+    //   var audioURI;
+    //   resolveLocalFileSystemURL(cordova.file.externalRootDirectory+"myrecording.mp3",function(entry) {
+    //     console.log('cdvfile URI: ' + entry.toInternalURL());
+    //     console.log('URI: ' + entry.toURL());
+    //     audioURI = entry.toURL();
+    //   }, function (err) {
+    //     console.log(err)
+    //   });
+
+    //   // var path = cordova.file.externalRootDirectory
+    //   // audioURI = path+this.mediaRec.src
+    //   console.log(audioURI)
+
+    //   var audioname = window.localStorage.getItem('USERID') + '_' + new Date().getTime() + 'test.jpg'
+    //   //上传接口
+    //   var uri = encodeURI(global.uploadUrl)
+    //   var options = {
+    //     fileKey: 'file',
+    //     fileName: audioname,
+    //     chunkedMode: true,
+    //     mimeType: 'audio/mpeg'
+    //   }
+    //   function success(r) {
+    //     console.log(r)
+    //     alert("上传成功! Code = " + r.responseCode);
+    //   }
+    //   function fail(error) {
+    //     alert("上传失败! Code = " + error.code);
+    //   }
+    //   var ft = new FileTransfer();
+    //   ft.upload(audioURI, uri, success, fail, options)
+      
+    // },
+    // startRecord(){
+    //   this.mediaRec.startRecord()
+    // },
+    // stopRecord(){
+    //   this.mediaRec.stopRecord()
+    // },
+    // start(){
+    //   this.mediaRec1.play()
+    // },
+    // stop(){
+    //   this.mediaRec1.stop()
+    // },
     returnA() {
       this.$router.push({name: '病人列表',params:{SELECTED:"病人"}});
     },
@@ -56,6 +147,7 @@ export default {
 
       function onSuccess(imageData) {
         document.getElementById('image').src =imageData;
+        console.log(imageData)
       }
 
       function onFail(message) {
@@ -66,6 +158,7 @@ export default {
     uploadPicture() {
       //获取元素中的图片uri
       var imgURI = document.getElementById('image').src
+      console.log(imgURI)
       //文件命名
       var photoname = window.localStorage.getItem('USERID') + '_' + new Date().getTime() + 'test.jpg'
       //上传接口
