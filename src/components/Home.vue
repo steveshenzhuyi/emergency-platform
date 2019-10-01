5a85a3cd312bc8556abb5a3f278b6b432e34e09b
zyhxym <zyhxym@gmail.com>
2019-10-02 02:43:34 +0800

    指挥端初始化

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div align="center">
     <img src="./pictrue/logo.png">
-    <h2>之江实验室</h2>
-    <h2>重大会议急救保障系统</h2><hr>
-    <mt-field label="用户名" placeholder="请输入用户名" v-model="userId"></mt-field>
+    <h2>第六届世界互联网大会</h2>
+    <h2>急救保障系统指挥端</h2><hr>
+    <!-- <mt-field label="用户名" placeholder="请输入用户名" v-model="userId"></mt-field> -->
     <mt-field label="密码" placeholder="请输入密码" type="password" v-model="pwd"></mt-field>
     <hr>
     <br>
@@ -24,146 +24,17 @@ export default {
     return {
       userId: '',
       pwd: '',
-      groupNo: '',
-      RoleCode:''
     };
   },
   methods: {
-    //先判断用户的小组
     check() {
-      console.log(this.userId)
-      axios.post('/getUserGroup',{
-        userId: this.userId
-      })
-      .then((response) => {
-        console.log(response);
-        if(response.data.results.length == 0) {
-          Toast('用户名错误！')
-          return;
-        } 
-        this.RoleCode = response.data.results[0].RoleCode
-        if (response.data.results[0].RoleCode == "R01") {
-          //现场组登录
-          this.groupNo = response.data.results[0].GroupNo;
-          window.localStorage.setItem('GROUPNO',this.groupNo);
-          var GN = this.groupNo;
-          console.log(response.data.results[0]);
-          axios.post('/checkPassword',{
-            userId: this.userId,
-            pwd: this.pwd
-          }).then((response) => {
-            if (response.data.results == "密码正确") {
-              window.localStorage.setItem('USERID',this.userId);
-              window.localStorage.setItem('ROLECODE',this.RoleCode);
-              this.$router.push({name: '病人列表',params:{SELECTED:"病人"}});
-            }else{
-              Toast('密码错误');
-            }
-          }).catch(function(error){
-            console.log("error",error);
-          });
-        } else if (response.data.results[0].RoleCode == "R02"){
-          //运输组登录
-          this.groupNo = response.data.results[0].GroupNo;
-          window.localStorage.setItem('GROUPNO',this.groupNo);
-          var GN = this.groupNo;
-          axios.post('/checkPassword',{
-            userId: this.userId,
-            pwd: this.pwd
-          }).then((response) => {
-            if (response.data.results == "密码正确") {
-              window.localStorage.setItem('USERID',this.userId);
-              window.localStorage.setItem('ROLECODE',this.RoleCode);
-  //             window.JPush.setTags({ sequence: 1, tags: [GN] },
-  // (result) => {
-  //   var sequence = result.sequence
-  //   var tags = result.tags  // 数组类型
-  //   // alert(sequence)
-  //   // alert(tags)
-  // }, (error) => {
-  //   var sequence = error.sequence
-  //   var errorCode = error.code
-  // })
-              this.$router.push({name: '转运列表',params:{SELECTED1:"病人"}});
-            }else{
-              // MessageBox.alert('用户名或密码错误!', '提示');
-              Toast('密码错误');
-              // alert("用户名或密码错误！");
-            }
-          }).catch(function(error){
-            console.log("error",error);
-          });
-        } else if (response.data.results[0].RoleCode == "R03"){
-          //医院端登录
-          this.groupNo = response.data.results[0].GroupNo;
-          window.localStorage.setItem('GROUPNO',this.groupNo);
-          var GN = this.groupNo;
-          axios.post('/checkPassword',{
-            userId: this.userId,
-            pwd: this.pwd
-          }).then((response) => {
-            if (response.data.results == "密码正确") {
-              window.localStorage.setItem('USERID',this.userId);
-              window.localStorage.setItem('ROLECODE',this.RoleCode);
-  //             window.JPush.setTags({ sequence: 1, tags: [GN] },
-  // (result) => {
-  //   var sequence = result.sequence
-  //   var tags = result.tags  // 数组类型
-  //   // alert(sequence)
-  //   // alert(tags)
-  // }, (error) => {
-  //   var sequence = error.sequence
-  //   var errorCode = error.code
-  // })
-              this.$router.push({name: '医院病人列表',params:{SELECTED2:"病人"}});
-            }else{
-              // MessageBox.alert('用户名或密码错误!', '提示');
-              Toast('密码错误');
-              // alert("用户名或密码错误");
-            }
-          }).catch(function(error){
-            console.log("error",error);
-          })
-        } else if (response.data.results[0].RoleCode == "R04"){
-          //专家端登录
-          this.groupNo = response.data.results[0].GroupNo;
-          window.localStorage.setItem('GROUPNO',this.groupNo);
-          var GN = this.groupNo;
-          axios.post('/checkPassword',{
-            userId: this.userId,
-            pwd: this.pwd
-          }).then((response) => {
-            if (response.data.results == "密码正确") {
-              window.localStorage.setItem('USERID',this.userId);
-              window.localStorage.setItem('ROLECODE',this.RoleCode);
-  //             window.JPush.setTags({ sequence: 1, tags: [GN] },
-  // (result) => {
-  //   var sequence = result.sequence
-  //   var tags = result.tags  // 数组类型
-  //   // alert(sequence)
-  //   // alert(tags)
-  // }, (error) => {
-  //   var sequence = error.sequence
-  //   var errorCode = error.code
-  // })
-              this.$router.push({name: '专家病人列表',params:{SELECTED2:"病人"}});
-            }else{
-              // MessageBox.alert('用户名或密码错误!', '提示');
-              Toast('密码错误');
-              // alert("用户名或密码错误");
-            }
-          }).catch(function(error){
-            console.log("error",error);
-          })
-        }
-      })
+     if(this.pwd == '123'){
+      this.$router.push({name: 'command'});
+     }else{
+      Toast('密码错误');
+     }
     },
-    hospital() {
-      this.$router.push({name: '医院病人列表'});
-    },
-    trans() {
-      this.$router.push({name: '转运列表'});
-    }
+    
   }
 };
 </script>
---
+++ src/components/command.vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+    <mt-tab-container class="page-tabbar-container" v-model="selected">
+      <mt-tab-container-item id="指挥">
+        <mt-header fixed style="font-size:25px;height: 50px;" title="视频通话">
+         <!--  <mt-button size="small" slot="left"
+            @click="$goRoute('/CPR')"><small>今日统计</small></mt-button>
+          <mt-button size="small" slot="right"
+            @click="$goRoute('/increaseA')"><small>新增病人</small></mt-button> -->
+          <hr>
+        </mt-header>
+        <br><br>  
+        <br><br>
+      </mt-tab-container-item>
+      <mt-tab-container-item id="地图">
+        <mt-header fixed style="font-size:25px;height: 50px;" title="地图实况">
+          <!-- <mt-button size="small" slot="right" @click="$goRoute('/increaseB')"><small>新增资源</small></mt-button> -->
+          <hr>
+        </mt-header>
+        <br><br>
+      </mt-tab-container-item>
+      <mt-tab-container-item id="统计">
+        <mt-header fixed style="font-size:25px;height: 50px;" title="信息统计">
+          <hr>
+        </mt-header>
+        <br><br>
+      </mt-tab-container-item>
+      <!-- <mt-tab-container-item id="个人">
+        <mt-header fixed style="font-size:25px;height: 50px;" title="个人信息">
+          <hr>
+        </mt-header>
+        <br><br><br>
+        <div style="text-align: left; margin-top: 10px">个人信息</div>
+        <mt-field label="姓名" v-model="Name" disabled></mt-field>
+        <mt-field label="性别" v-model="Gender" disabled></mt-field>
+        <mt-field label="年龄" v-model="Age" disabled></mt-field>
+        <mt-field label="手机" v-model="Phone" disabled></mt-field>
+        <mt-field label="邮箱" v-model="Email" disabled></mt-field>
+        <mt-field label="职称" v-model="TitleName" disabled></mt-field>
+        <mt-field label="科室" v-model="DepartmentName" disabled></mt-field><hr>
+        <div style="text-align: left; margin-top: 10px">角色：现场组</div>
+        <mt-field label="所属小组" v-model="GroupName" disabled></mt-field>
+        <mt-field label="组内职务" v-model="GroupPosition" disabled></mt-field>
+        <mt-field label="责任区域" v-model="ManageArea" disabled></mt-field>
+        <mt-field label="重点保障对象" v-model="GuaranteeObject" disabled></mt-field><hr>
+        <mt-button size="large" type="danger" @click="logout()">退出登录</mt-button><br><br><br><br>
+      </mt-tab-container-item> -->
+    </mt-tab-container>
+    <div>
+      <mt-tabbar v-model= "selected" fixed>
+        <mt-tab-item id="指挥">
+          <img slot="icon" src="./icon/语音通话.png"><div style="font-size:12px">指挥</div>
+        </mt-tab-item>
+        <mt-tab-item id="地图">
+         <img slot="icon" src="./icon/本车.png"><div style="font-size:12px">地图</div>
+        </mt-tab-item>
+        <mt-tab-item id="统计">
+          <img slot="icon" src="./icon/病人.png"><div style="font-size:12px">统计</div>
+        </mt-tab-item>
+       <!--  <mt-tab-item id="个人">
+          <img slot="icon" src="./icon/个人.png"><div style="font-size:12px">个人</div> -->
+        </mt-tab-item>
+      </mt-tabbar>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import {picker} from 'mint-ui';
+import axios from 'axios';
+import { Toast } from 'mint-ui';
+import { MessageBox } from 'mint-ui';
+export default {
+  // inject:['reload'],
+  data() {
+    return {
+     
+    };
+  },
+  mounted() {
+    console.log();
+  },
+  methods: {
+   
+  },
+};
+</script>
+
+<style>
+  .I级{
+    color:red;
+  }
+  .II级{
+    color:orange;
+  }
+  .III级{
+    color:#E6BD1A;
+  }
+  .IV级{
+    color:blue;
+  }
+  .V级{
+    color:blue;
+  }
+  .处置完成{
+    color:green;
+  }
+  .已后送{
+    color:green;
+  }
+  .待后送{
+    color:red;
+  }
+  .处置中{
+    color:red;
+  }
+</style>
