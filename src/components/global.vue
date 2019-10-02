056398fd42240202d8be9c174439fd6335b52269
zyhxym <zyhxym@gmail.com>
2019-10-02 16:19:32 +0800

    服务器迁移

--- src/components/global.vue
+++ src/components/global.vue
@@ -1,8 +1,12 @@
 <script type="text/javascript">
     // 定义一些公共的属性和方法
-    var photoUrl = 'http://101.132.140.25:8001/photos/'
-    var uploadUrl = "http://101.132.140.25:3000/upload"
-    var facecheck = "http://101.132.140.25:3000/facecheck"
+    // var photoUrl = 'http://101.132.140.25:8001/photos/'
+    // var uploadUrl = "http://101.132.140.25:3000/upload"
+    // var facecheck = "http://101.132.140.25:3000/facecheck"
+    var photoUrl = 'http://47.98.252.131:8001/photos/'
+    var uploadUrl = "http://47.98.252.131:3000/upload"
+    var facecheck = "http://47.98.252.131:3000/facecheck"
+    
     var showVideoAlert = false
     function changeVideoAlert(now) {
         this.showVideoAlert = now
